a014b4d8d8a2f7524848c5ea6beb1b19358d93aa
pvasyani <payal.vasyani7@gmail.com>
2026-03-12 16:47:13 +0530

    Initial Commit - Angular Microfrontend Project

---
+++ projects/shell/src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    { 
+        path: 'product', 
+        loadComponent: () => loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4201/remoteEntry.js',
+            exposedModule: './Component'
+        }).then(m => m.AppComponent),   
+    },
+    {
+        path: 'account', 
+        loadComponent: () => loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:4202/remoteEntry.js',
+            exposedModule: './Component'
+        }).then(m => m.AppComponent),   
+    },
+    { path : '', redirectTo: 'product', pathMatch: 'full' },
+];
